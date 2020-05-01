3cea79a007bcfec283d1897839a41c677a608a60
ellenluong <ellenluong01@gmail.com>
2020-05-01 23:54:45 +0700

    add JOIN p2

--- SQLzoo_JOIN.sql
+++ SQLzoo_JOIN.sql
@@ -1,3 +1,5 @@
+-- Part 1: 
+-- The data is available (mysql format) at http://sqlzoo.net/euro2012.sql
 -- 1. Show the matchid and player name for all goals scored by Germany
 SELECT matchid, player 
   FROM goal 
@@ -77,3 +79,45 @@ SELECT mdate,
 FROM game LEFT JOIN goal ON goal.matchid = game.id
 GROUP BY game.id
 ORDER BY mdate, matchid, team1, team2
+
+-- Part 2:
+-- The database consists of three tables movie , actor and casting
+-- 10. List the films together with the leading star for all 1962 films
+SELECT movie.title, actor.name
+FROM movie
+JOIN casting ON movieid=movie.id
+JOIN actor ON actorid=actor.id
+WHERE yr=1962
+AND ord=1
+
+-- 11. Which were the busiest years for 'Rock Hudson'.
+-- Show the year and the number of movies he made each year for any year 
+-- in which he made more than 2 movies
+SELECT yr, COUNT(title) 
+FROM movie JOIN casting ON movie.id=movieid
+           JOIN actor   ON actorid=actor.id
+WHERE name='Rock Hudson'
+GROUP BY yr
+HAVING COUNT(title) > 2
+
+-- 12. List the film title and the leading actor 
+-- for all of the films 'Julie Andrews' played in
+-- tip: Film title is not unique, go with movie id
+SELECT title, name
+FROM casting
+JOIN movie ON (movieid=movie.id)
+JOIN actor ON (actorid=actor.id AND ord=1)
+WHERE movieid IN(
+    SELECT movieid FROM casting
+    JOIN actor ON actorid=actor.id
+    WHERE name ='Julie Andrews')
+
+-- OR:
+SELECT title, name
+FROM casting
+JOIN movie ON (movieid=movie.id)
+JOIN actor ON (actorid=actor.id AND ord=1)
+WHERE movieid IN(
+    SELECT movieid FROM casting
+    WHERE actorid IN (SELECT actor.id FROM actor
+                      WHERE name ='Julie Andrews'))
